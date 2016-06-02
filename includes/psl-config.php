d89bf637db5860963879a37113069b53bf98964a
Robert Hartnett <rhartnett35@gmail.com>
2016-06-02 16:47:31 -0400

    Added php config file

---
+++ includes/psl-config.php
@@ -0,0 +1,17 @@
+<?php
+//MySQL Details
+$DB_Host = "HOST";
+$DB_User = "USER";
+$DB_Pass = "PASSWORD";
+$DB = "DATABASE";
+
+//Contact name
+$sysadmin = "Robert";
+
+//Corp/Alliance name
+$ENT_Name = "Testcraft";
+
+//Unless in development, set to true
+define("SECURE", False)
+
+?>
