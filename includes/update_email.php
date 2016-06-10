5ae3b4dfb09d83b87ec2c579be251a2906d62752
Robert Hartnett <rhartnett35@gmail.com>
2016-06-10 15:01:45 -0400

    Update update_email.php

--- includes/update_email.php
+++ includes/update_email.php
@@ -1,6 +1,17 @@
 <?php
 include_once 'db_connect.php';
 include_once 'psl-config.php';
+include_once 'functions.php';
+
+
+sec_session_start();
+if (login_check($conn) == true) {
+    $logged = 'in';
+} else {
+    $logged = 'out';
+	header("Location: ./login.php");
+	end();
+}
 
 $id = $_POST['id'];
 $email = $_POST['email'];
